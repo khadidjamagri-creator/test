7c0b1e99dde46864d0663f06c025792034659165
khadidjamagri-creator <khadidja.magri@univ-bouira.dz>
2026-03-17 01:15:50 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(df["email"])
+y = df["label"]
+model = MultinomialNB()
+model.fit(X, y)
